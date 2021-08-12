c5d98a64d1a7f7934dc2a71ca22d35bae00e3228
VeraSv <47688337+VeraSv@users.noreply.github.com>
2021-08-12 17:44:21 +0300

    created console application

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число строк: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число столбцов: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int[,] mas = new int[num1, num2];
+            for (int i = 0; i < num1; i++)
+            {
+                for (int j = 0; j < num2; j++)
+                {
+                    Random rnd = new Random();
+
+                    mas[i,j] = rnd.Next(10, 99);
+                    Console.Write($"{mas[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
